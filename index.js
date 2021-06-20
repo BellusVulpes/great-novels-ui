e470238d22780738e9c05a6e5a21dcec2f3abe2c
christopher boucher <chrismichb@yahoo.com>
2021-06-20 18:19:27 -0400

    added app.use and app.all to index.js

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express')
+const path = require('path')
 const { getAllAuthors, getAuthorByIdOrName } = require('./controllers/authors')
 const { getAllGenres, getGenreById } = require('./controllers/genres')
 const { getAllNovels, getNovelByIdOrTitle } = require('./controllers/novels')
 
 const app = express()
 
-app.get('/authors', getAllAuthors)
-app.get('/authors/:identifier', getAuthorByIdOrName)
+app.use(express.static('public'))
 
-app.get('/genres', getAllGenres)
-app.get('/genres/:id', getGenreById)
+app.get('/api/authors', getAllAuthors)
+app.get('/api/authors/:identifier', getAuthorByIdOrName)
 
-app.get('/novels', getAllNovels)
-app.get('/novels/:identifier', getNovelByIdOrTitle)
+app.get('/api/genres', getAllGenres)
+app.get('/api/genres/:id', getGenreById)
 
-app.listen(1337, () => {
-  console.log('Listening on port 1337...') // eslint-disable-line no-console
+app.get('/api/novels', getAllNovels)
+app.get('/api/novels/:identifier', getNovelByIdOrTitle)
+
+app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'public', 'index.html')))
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080...') // eslint-disable-line no-console
 })
