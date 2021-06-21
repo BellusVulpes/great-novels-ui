447aa64a823514cae25519ed4cf4769fa29338c5
christopher boucher <chrismichb@yahoo.com>
2021-06-20 23:17:04 -0400

    created first useEffect in App.js in src folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Novels from './components/great-novels'
+import {fetchData, filtered} from './utils/great-novels'
 
 function App() {
+  const [novelsData, setNovelsData] = useState([])
+  const [filteredNovels, setFilteredNovels] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  useEffect(() => {
+    async function pullData() {
+      const data = await fetchData()
+      setNovelsData(data)
+      setFilteredNovels(data)
+    }
+    pullData()
+  }, )
   return (
     <div className="App">
-      Chris's React App
+      <Novels/>
     </div>
   );
 }
